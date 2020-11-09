c260f11d4f543b21b4e7b99f08a9fa1add7e1120
Johan Lidberg <johan.lidberg@skola.botkyrka.se>
2020-11-09 14:16:20 +0100

    4

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Read instructions. And remove this line.
+function randomInt(){
+    return (Math.floor(Math.random() * 100))
+
+}
+
+let secretNumber = randomInt(100)
+
+
+function getUserGuess () {
+    const stringValue = document.getElementById('user-input').value
+    return parseInt(stringValue, 10)
+}
+console.log()
+
+document.addEventListener('keyup', function (event) {
+    if (event.key === 'Enter') {
+        const guess = getUserGuess
+ 
+        if (guess() > secretNumber) {
+            setMessage("För högt")
+        }
+        else if(guess() == secretNumber) {
+            setMessage("Rätt!")
+        }
+        else {
+            setMessage("För lågt")
+        }
+        {
+            clearInput()
+        }
+
+    }
+})
+
+function setMessage(msg) {
+    const message = document.getElementById("message")
+    message.innerHTML = msg
+}
+
+function clearInput (){
+    document.getElementById('user-input').value = ''
+    
+
+    
+}
+  
